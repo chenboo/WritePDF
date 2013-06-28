40be1f2b579a0d03e1f21469a45e5347e25b9ece
chenbo <galaxy.chen@qq.com>
2013-06-28 18:04:47 +0800

    1:dummy

--- WritePDF/WritePDF.cpp
+++ WritePDF/WritePDF.cpp
@@ -17,7 +17,12 @@
 #include <string.h>
 #include <exception>
 #include "./include/hpdf.h"
+#include "algorithm"
 
+int max(int x, int y)
+{
+	return x > y ? x : y;
+}
 #ifdef HPDF_DLL
 void  __stdcall
 #else
@@ -178,6 +183,45 @@ void DrawTable(int x, int y, int width, int height)
 
 }
 
+void DrawString(HPDF_Page page, int x, int y, int width, const char* szInfo, HPDF_TextAlignment align)
+{
+	HPDF_Rect rect;
+	rect.left = x;
+	rect.top = y;
+	rect.right = rect.left + width;
+	rect.bottom = rect.top + 20;
+
+	HPDF_Page_BeginText (page);
+	HPDF_Page_TextRect (page, rect.left, rect.top, rect.right, rect.bottom,
+		szInfo, align, NULL);
+	HPDF_Page_EndText (page);
+}
+
+void DrawString(HPDF_Page page, int x, int y, const char* szInfo, HPDF_TextAlignment align)
+{
+	int nStrWidth = HPDF_Page_TextWidth (page, szInfo);
+
+	HPDF_Rect rect;
+	rect.left = x;
+	rect.top = y;
+	rect.right = rect.left + nStrWidth + 10;
+	rect.bottom = rect.top + 20;
+
+	HPDF_Page_BeginText (page);
+	HPDF_Page_TextRect (page, rect.left, rect.top, rect.right, rect.bottom,
+		szInfo, align, NULL);
+	HPDF_Page_EndText (page);
+}
+
+void DrawString(HPDF_Page page, HPDF_Rect rect, const char* szInfo, HPDF_TextAlignment align)
+{
+	HPDF_Page_BeginText (page);
+	HPDF_Page_TextRect (page, rect.left, rect.top, rect.right, rect.bottom,
+		szInfo, align, NULL);
+	HPDF_Page_EndText (page);
+}
+
+
 int main (int argc, char **argv)
 {
 	const char *page_title = "哎呦 我去!@#$@#%$^%&\n*^*^&x%%x。";
@@ -186,8 +230,8 @@ int main (int argc, char **argv)
     HPDF_Page page;
     HPDF_Font def_font;
     HPDF_REAL tw;
-    HPDF_REAL height;
-    HPDF_REAL width;
+    HPDF_REAL nPageHeight;
+    HPDF_REAL nPageWidth;
     HPDF_UINT i;
 
     //strcpy (fname, argv[0]);
@@ -203,13 +247,16 @@ int main (int argc, char **argv)
         /* Add a new page object. */
         page = HPDF_AddPage (pdf);
 
-        height = HPDF_Page_GetHeight (page);
-        width = HPDF_Page_GetWidth (page);
+		HPDF_Page_SetWidth(page, 800);
+		HPDF_Page_SetHeight(page, 1200);
+
+        nPageHeight = HPDF_Page_GetHeight (page);
+        nPageWidth = HPDF_Page_GetWidth (page);
 
         /* Print the lines of the page. */
-        HPDF_Page_SetLineWidth (page, 1);
-        HPDF_Page_Rectangle (page, 50, 50, width - 100, height - 110);
-        HPDF_Page_Stroke (page);
+        //HPDF_Page_SetLineWidth (page, 1);
+        //HPDF_Page_Rectangle (page, 50, 50, width - 100, height - 110);
+        //HPDF_Page_Stroke (page);
 
 		HPDF_UseCNSFonts(pdf);
 		HPDF_UseCNSEncodings(pdf);
@@ -218,44 +265,145 @@ int main (int argc, char **argv)
         def_font = HPDF_GetFont (pdf, "SimSun", "GBK-EUC-H");
         HPDF_Page_SetFontAndSize (page, def_font, 10);
 
+		char* szLable1 = "学生名:";
+		char* szLable2 = "考试名称:";
+		char* szLable3 = "班级名称:";
+		char* szLable4 = "考试日期:";
+		char* szLable5 = "考试时间:";
 
-		print_grid(pdf, page);
+		char* szInfo1 = "chenbo";
+		char* szInfo2 = "quiz 1";
+		char* szInfo3 = "class mode 1";
+		char* szInfo4 = "18:00 2012-06-27";
+		char* szInfo5 = "120 min";
 
-        tw = HPDF_Page_TextWidth (page, page_title);
-        HPDF_Page_BeginText (page);
-        HPDF_Page_MoveTextPos (page, (width - tw) / 2, height - 50);
-        HPDF_Page_ShowText (page, page_title);
-        HPDF_Page_EndText (page);
+		int nLable1Width = HPDF_Page_TextWidth(page, szLable1);
+		int nLable2Width = HPDF_Page_TextWidth(page, szLable2);
+		int nLable3Width = HPDF_Page_TextWidth(page, szLable3);
+		int nLable4Width = HPDF_Page_TextWidth(page, szLable4);
+		int nLable5Width = HPDF_Page_TextWidth(page, szLable5);
 
-        /* output subtitle. */
-        HPDF_Page_BeginText (page);
-        HPDF_Page_MoveTextPos (page, (width - tw) / 2, height - 70);
-        HPDF_Page_SetFontAndSize (page, def_font, 10);
-        HPDF_Page_ShowText (page, "哎呦 哎呦");
-        HPDF_Page_EndText (page);
+		int nInfo1Width = HPDF_Page_TextWidth(page, szInfo1);
+		int nInfo2Width = HPDF_Page_TextWidth(page, szInfo2);
+		int nInfo3Width = HPDF_Page_TextWidth(page, szInfo3);
+		int nInfo4Width = HPDF_Page_TextWidth(page, szInfo4);
+		int nInfo5Width = HPDF_Page_TextWidth(page, szInfo5);
 
-        //HPDF_Page_BeginText (page);
-        //HPDF_Page_MoveTextPos (page, 60, height - 105);
+		int nLableMaxWidth = max(max(max(nLable1Width, nLable2Width), max(nLable3Width, nLable4Width)), nLable5Width);
+		int nInfoMaxWidth = max(max(max(nInfo1Width, nInfo2Width), max(nInfo3Width, nInfo4Width)), nInfo5Width);
 
-        //i = 0;
-        //while (font_list[i]) {
-        //    const char* samp_text = "abcdefgABCDEFG12345!#$%&+-@?";
-        //    HPDF_Font font = HPDF_GetFont (pdf, font_list[i], NULL);
+		HPDF_Rect rectLable;
+		rectLable.left = (nPageWidth - nInfoMaxWidth - nLableMaxWidth - 20) / 2;
+		rectLable.top = nPageHeight - 20;
+		rectLable.right = rectLable.left + nLableMaxWidth;
+		rectLable.bottom = rectLable.top - 20;
+	
+		DrawString(page, rectLable, szLable1, HPDF_TALIGN_RIGHT);
 
-        //    /* print a label of text */
-        //    HPDF_Page_SetFontAndSize (page, def_font, 9);
-        //    HPDF_Page_ShowText (page, font_list[i]);
-        //    HPDF_Page_MoveTextPos (page, 0, -18);
+		rectLable.top -= 15;
+		rectLable.bottom -= 15;
+		DrawString(page, rectLable, szLable2, HPDF_TALIGN_RIGHT);
 
-        //    /* print a sample text. */
-        //    HPDF_Page_SetFontAndSize (page, font, 20);
-        //    HPDF_Page_ShowText (page, samp_text);
-        //    HPDF_Page_MoveTextPos (page, 0, -20);
+		rectLable.top -= 15;
+		rectLable.bottom -= 15;
+		DrawString(page, rectLable, szLable3, HPDF_TALIGN_RIGHT);
+		
+		rectLable.top -= 15;
+		rectLable.bottom -= 15;
+		DrawString(page, rectLable, szLable4, HPDF_TALIGN_RIGHT);
+		
+		rectLable.top -= 15;
+		rectLable.bottom -= 15;
+		DrawString(page, rectLable, szLable5, HPDF_TALIGN_RIGHT);
 
-        //    i++;
-        //}
+		HPDF_Page_SetFontAndSize (page, def_font, 9);
 
-        //HPDF_Page_EndText (page);
+		HPDF_Rect rectInfo;
+		rectInfo.left = rectLable.right + 10;
+		rectInfo.top = nPageHeight - 20;
+		rectInfo.right = rectInfo.left + nInfoMaxWidth;
+		rectInfo.bottom = rectInfo.top - 20;
+
+		DrawString(page, rectInfo, szInfo1, HPDF_TALIGN_LEFT);
+
+		rectInfo.top -= 15;
+		rectInfo.bottom -= 15;
+		DrawString(page, rectInfo, szInfo2, HPDF_TALIGN_LEFT);
+
+		rectInfo.top -= 15;
+		rectInfo.bottom -= 15;
+		DrawString(page, rectInfo, szInfo3, HPDF_TALIGN_LEFT);
+
+		rectInfo.top -= 15;
+		rectInfo.bottom -= 15;
+		DrawString(page, rectInfo, szInfo4, HPDF_TALIGN_LEFT);
+
+		rectInfo.top -= 15;
+		rectInfo.bottom -= 15;
+		DrawString(page, rectInfo, szInfo5, HPDF_TALIGN_LEFT);
+		
+		
+		print_grid(pdf,page);
+//////////////////////////////////////////////////////////////////////////////////////
+		HPDF_Rect rectTable;
+		rectTable.left = 100;
+		rectTable.top = rectInfo.bottom - 40;
+		rectTable.right = nPageWidth - 100;
+		rectTable.bottom = rectTable.top - 20 * 3;
+
+		HPDF_Page_Rectangle (page, rectTable.left, rectTable.top, rectTable.right - rectTable.left,
+			rectTable.bottom - rectTable.top);
+		HPDF_Page_Stroke (page);
+
+		//line
+		HPDF_Page_MoveTo (page, rectTable.left, rectTable.top - 20);
+		HPDF_Page_LineTo (page, rectTable.right, rectTable.top - 20);
+		HPDF_Page_Stroke (page);
+		
+		HPDF_Page_MoveTo (page, rectTable.left, rectTable.top - 40);
+		HPDF_Page_LineTo (page, rectTable.right, rectTable.top - 40);
+		HPDF_Page_Stroke (page);
+		
+		HPDF_Page_MoveTo (page, rectTable.left, rectTable.top - 60);
+		HPDF_Page_LineTo (page, rectTable.right, rectTable.top - 60);
+		HPDF_Page_Stroke (page);
+
+		//column
+		HPDF_Page_MoveTo (page, rectTable.left + (rectTable.right - rectTable.left) / 3, rectTable.top);
+		HPDF_Page_LineTo (page, rectTable.left + (rectTable.right - rectTable.left) / 3, rectTable.bottom);
+		HPDF_Page_Stroke (page);
+
+		//HPDF_Page_MoveTo (page, rectTable.left, rectTable.top - 40);
+		//HPDF_Page_LineTo (page, rectTable.right, rectTable.top - 40);
+		//HPDF_Page_Stroke (page);
+
+		//HPDF_Page_MoveTo (page, rectTable.left, rectTable.top - 60);
+		//HPDF_Page_LineTo (page, rectTable.right, rectTable.top - 60);
+		//HPDF_Page_Stroke (page);
+
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         HPDF_SaveToFile (pdf, fname);
 
